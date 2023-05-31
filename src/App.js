c2402aa48bc120a8a7a3ad7b539aad9e6e7f67cf
lbferrand <lbferrand@gmail.com>
2023-05-31 10:44:36 -0400

    Install React Router Dom and import it

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import AboutPage from './Pages/AboutPage';
+import ArticlePage from './Pages/ArticlePage';
+import ArticlesListPage from './Pages/ArticlesListPage';
 
 function App() {
   return (
-    <div className='container'>
-      <div className="App">
-        <h1>My Awesome Blog</h1>
-        <div id='page-body'>
-          Welcome to my blog!
+    <BrowserRouter>
+      <div className='container'>
+        <div className="App">
+          <h1>My Awesome Blog</h1>
+          <div id='page-body'>
+            <HomePage />
+            <AboutPage />
+            <ArticlesListPage />
+            <ArticlePage />
+          </div>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
+    
     
   );
 }
