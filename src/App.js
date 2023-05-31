13d97c71cbecb76c2066b81a13a707f051ec9a89
lbferrand <lbferrand@gmail.com>
2023-05-31 10:19:15 -0400

    Create pages as components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Awesome Blog</h1>
-      <div id='page-body'>
-        Welcome to my blog!
+    <div className='container'>
+      <div className="App">
+        <h1>My Awesome Blog</h1>
+        <div id='page-body'>
+          Welcome to my blog!
+        </div>
       </div>
     </div>
+    
   );
 }
 
