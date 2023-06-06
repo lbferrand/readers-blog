46cf199d04a8c82a892db20360ccc3d0128a187d
lbferrand <lbferrand@gmail.com>
2023-06-06 14:57:09 -0400

    Add Comments form & display comments

--- src/Pages/ArticlePage.js
+++ src/Pages/ArticlePage.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import NotFoundPage from './NotFoundPage';
 import articles from './article-content';
+import CommentsList from '../components/CommentsList';
+import AddCommentForm from '../components/AddCommentForm';
 
 const ArticlePage = () => {
     const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
@@ -20,6 +22,12 @@ const ArticlePage = () => {
     }, []);
 
     const article = articles.find(article => article.name === articleId);
+    
+    const addUpvote = async () => {
+      const response = await axios.put(`/api/articles/${articleId}/upvote`);
+      const updatedArticle = response.data;
+      setArticleInfo(updatedArticle);
+    }
 
     if (!article) {
         return <NotFoundPage />
@@ -28,10 +36,18 @@ const ArticlePage = () => {
     return (
         <>
         <h1>{article.title}</h1>
-        <p>This article has {articleInfo.upvotes} upvote(s)</p>
+        <div className="upvotes-sections">
+          <button onClick={addUpvote}>Upvote</button>
+          <p>This article has {articleInfo.upvotes} upvote(s)</p>
+        </div>
+        
         {article.content.map((paragraph, i) => (
             <p key={i}>{paragraph}</p>
         ))}
+        <AddCommentForm
+          articleName={articleId}
+          onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+        <CommentsList comments={articleInfo.comments} />
         </>
     );
 }
