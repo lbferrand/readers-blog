0a00b9a281de3ddf1812ca970fb09b475dba0531
lbferrand <lbferrand@gmail.com>
2023-06-07 19:46:38 -0400

    Add authentication to login and create account

--- src/Pages/ArticlePage.js
+++ src/Pages/ArticlePage.js
@@ -1,55 +1,67 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import NotFoundPage from './NotFoundPage';
-import articles from './article-content';
-import CommentsList from '../components/CommentsList';
-import AddCommentForm from '../components/AddCommentForm';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import NotFoundPage from "./NotFoundPage";
+import articles from "./article-content";
+import CommentsList from "../components/CommentsList";
+import AddCommentForm from "../components/AddCommentForm";
+import useUser from "../hooks/useUser";
 
 const ArticlePage = () => {
-    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
-    const { articleId } = useParams();
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+  const { articleId } = useParams();
 
-    useEffect(() => {
-        const loadArticleInfo = async () => {
-            const response = await axios.get(`/api/articles/${articleId}`);
-            const newArticleInfo = response.data;
-            setArticleInfo(newArticleInfo);
-        }
+  const { user, isLoading } = useUser();
 
-        loadArticleInfo();
+  useEffect(() => {
+    const loadArticleInfo = async () => {
+      const response = await axios.get(`/api/articles/${articleId}`);
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo);
+    };
+
+    loadArticleInfo();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  }, []);
 
-    const article = articles.find(article => article.name === articleId);
-    
-    const addUpvote = async () => {
-      const response = await axios.put(`/api/articles/${articleId}/upvote`);
-      const updatedArticle = response.data;
-      setArticleInfo(updatedArticle);
-    }
+  const article = articles.find((article) => article.name === articleId);
 
-    if (!article) {
-        return <NotFoundPage />
-    }
+  const addUpvote = async () => {
+    const response = await axios.put(`/api/articles/${articleId}/upvote`);
+    const updatedArticle = response.data;
+    setArticleInfo(updatedArticle);
+  };
 
-    return (
-        <>
-        <h1>{article.title}</h1>
-        <div className="upvotes-sections">
+  if (!article) {
+    return <NotFoundPage />;
+  }
+
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <div className="upvotes-sections">
+        {user ? (
           <button onClick={addUpvote}>Upvote</button>
-          <p>This article has {articleInfo.upvotes} upvote(s)</p>
-        </div>
-        
-        {article.content.map((paragraph, i) => (
-            <p key={i}>{paragraph}</p>
-        ))}
+        ) : (
+          <button>Log in to upvote</button>
+        )}
+        <p>This article has {articleInfo.upvotes} upvote(s)</p>
+      </div>
+
+      {article.content.map((paragraph, i) => (
+        <p key={i}>{paragraph}</p>
+      ))}
+      {user ? (
         <AddCommentForm
           articleName={articleId}
-          onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
-        <CommentsList comments={articleInfo.comments} />
-        </>
-    );
-}
+          onArticleUpdated={(updatedArticle) => setArticleInfo(updatedArticle)}
+        />
+      ) : (
+        <button>Log in to add a comment</button>
+      )}
+      <CommentsList comments={articleInfo.comments} />
+    </>
+  );
+};
 
 export default ArticlePage;
