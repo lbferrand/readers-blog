a3eb7f3f033ab4079cd6aa8245c273b9f5cbd797
lbferrand <lbferrand@gmail.com>
2023-06-08 09:25:55 -0400

    Update NavBar.js

--- src/Pages/NavBar.js
+++ src/Pages/NavBar.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
 import useUser from "../hooks/useUser";
 
 const NavBar = () => {
   const { user } = useUser();
+  const navigate = useNavigate();
+
   return (
     <nav>
       <ul>
@@ -18,7 +21,23 @@ const NavBar = () => {
         </li>
       </ul>
       <div className="nav-right">
-        {user ? <button>Log Out</button> : <button>Log In</button>}
+        {user ? (
+          <button
+            onClick={() => {
+              signOut(getAuth());
+            }}
+          >
+            Log Out
+          </button>
+        ) : (
+          <button
+            onClick={() => {
+              navigate("/login");
+            }}
+          >
+            Log In
+          </button>
+        )}
       </div>
     </nav>
   );
